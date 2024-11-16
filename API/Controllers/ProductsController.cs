a9421157c72d4a5d65a1a12ed47e72ff22c2d8f8
Ma Latif <latif127@hotmail.com>
2024-11-16 16:01:19 -0500

    End of section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -11,19 +11,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController(IGenericRepository<Product> repo):ControllerBase
+    
+    public class ProductsController(IGenericRepository<Product> repo) : BaseApiController
     {
-        
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, 
-            string? type, string? sort) {
-               var spec = new ProductSpecification(brand, type, sort);
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts([FromQuery]ProductSpecParams specParams) {
+               var spec = new ProductSpecification(specParams);
 
                var products = await repo.ListAsync(spec);
+               var count=await repo.CountAsync(spec);
 
-               return Ok(products);
+               return await CreatePagedResult(repo, spec, specParams.PageIndex, specParams.PageSize);
          }
 
          [HttpGet("{id:int}")] // api/products/2
--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -10,6 +10,7 @@ namespace Core.Specifications
     public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecification<T>
     {
         protected BaseSpecification():this(null){}
+
         public Expression<Func<T, bool>>? Criteria => criteria;
 
         public Expression<Func<T, object>>? OrderBy {get; private set;}
@@ -17,6 +18,18 @@ namespace Core.Specifications
         public Expression<Func<T, object>>? OrderByDescending {get; private set;}
 
         public bool IsDistinct {get; private set;}
+
+        public int Take {get; private set;}
+        public int Skip {get; private set;}
+        public bool IsPagingEnabled {get; private set;}
+
+        public IQueryable<T> ApplyCriteria(IQueryable<T> query){
+            if (Criteria!=null){
+                query=query.Where(Criteria);
+            }
+
+            return query;
+        }
 
         protected void AddOrderBy(Expression<Func<T, object>> orderByExpression){
             OrderBy=orderByExpression;
@@ -27,6 +40,12 @@ namespace Core.Specifications
 
         protected void ApplyDistinct(){
             IsDistinct=true;
+        }
+
+        protected void ApplyPaging(int skip, int take){
+            Skip=skip;
+            Take=take;
+            IsPagingEnabled=true;
         }
     }
 
