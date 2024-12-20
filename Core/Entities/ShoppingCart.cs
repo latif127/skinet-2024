4212a5c2c6954f7e4bc0bb4818e8eea772394dcb
Ma Latif <latif127@hotmail.com>
2024-12-19 21:15:11 -0500

    End of section 12

---
+++ Core/Entities/ShoppingCart.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class ShoppingCart
+    {
+        public required string Id { get; set; }
+        public List<CartItem> Items { get; set; }=[];
+    }
+}
